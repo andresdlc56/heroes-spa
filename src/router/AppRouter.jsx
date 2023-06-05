db3a757a40955cf87e12925f28f279fae8941087
Andres de la Cruz <andresdlc56@gmail.com>
2023-06-05 15:24:20 -0400

    Rutas Publicas y Privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,30 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 
-import { Navbar } from "../ui";
 import { HeroesRoutes,  } from "../heroes";
 import { LoginPage } from "../auth";
+import { PrivateRoute, PublicRoute } from "./";
+
 
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="login" element={ <LoginPage /> } />
+                <Route path="login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } />
 
-                <Route path="/*" element={ <HeroesRoutes /> } />
+                {/*<Route path="login" element={ <LoginPage /> } />*/}
+
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>  
+                } />
+
+                {/*<Route path="/*" element={ <HeroesRoutes /> } />*/}
             </Routes>
         </>
     )
