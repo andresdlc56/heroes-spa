8986b70257fcb77d83b7e79becbb9b5679a52fa6
Andres de la Cruz <andresdlc56@gmail.com>
2023-06-07 10:11:10 -0400

    cambiando ruta de la img de los heroes

--- src/heroes/pages/HeroePage.jsx
+++ src/heroes/pages/HeroePage.jsx
@@ -28,7 +28,7 @@ export const HeroePage = () => {
         <div className="row mt-5">
             <div className="col-4">
                 <img 
-                    src={`/assets/heroes/${ heroe.id }.jpg`} 
+                    src={`/heroes/${ heroe.id }.jpg`} 
                     alt={ heroe.superhero } 
                     className="img-thumbnail animate__animated animate__fadeInLeft"
                 />
